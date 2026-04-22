c621656c93233df64b710eaf13e763725c544807
Noritaka <nori.ttt11@gmail.com>
2026-04-23 00:31:25 +0900

    Update homePage.js

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,16 +5,24 @@ export const homePage = {
   render(context) {
     return `
 
+    <section class="quick-grid quick-grid--home">
+      ${renderQuickLink('マップ', '/map', 'Map', "assets/img/map.png")}
+      ${renderQuickLink('企画', '/projects', 'Projects', "assets/img/attractions.png")}
+      ${renderQuickLink('ステージ公演', '/stage', 'Stage events', "assets/img/event.png")}
+      ${renderQuickLink('飲食', '/food', 'Foods', "assets/img/food.png")}
+    </section>
 
-      <section class="quick-grid quick-grid--home">
-        ${ renderQuickLink('マップ', '/map', '地図から企画やトイレ等の検索が可能です。', "assets/img/map.png")}
-        ${renderQuickLink('企画', '/projects', '全ての企画から絞り込み検索が可能です。', "assets/img/attractions.png")}
-        ${renderQuickLink('ステージ公演', '/stage', '公演の時間と場所の確認をすることができます。', "assets/img/event.png")}
-        ${renderQuickLink('飲食', '/food', 'あああああ。', "assets/img/food.png")}
-        ${renderQuickLink('お気に入り', '/favorites', '保存した企画', "assets/img/map.png")}
-      </section>
+    <div class="slider">
+      <div class="slider-track" id="sliderTrack">
+      <img src="assets/img/1.jpg" alt="画像1">
+      <img src="assets/img/2.jpg" alt="画像2">
+      <img src="assets/img/3.jpg" alt="画像3">
+    </div>
+</div>
     `;
   },
 
   bind() {},
 };
+
+//   ${renderQuickLink('お気に入り', '/favorites', '保存した企画', "assets/img/map.png")}
