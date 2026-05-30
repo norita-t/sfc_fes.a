f8992503c98192b12998f87e34db3cd3fe8fc120
Noritaka <nori.ttt11@gmail.com>
2026-05-30 23:43:56 +0900

    Delete src directory

--- src/pages/notFoundPage.js
+++
@@ -1,8 +0,0 @@
-import { renderEmptyState } from '../components/ui.js';
-
-export const notFoundPage = {
-  render() {
-    return renderEmptyState('ページが見つかりません', 'ホームに戻ってもう一度選び直してください。', '/', 'ホームへ');
-  },
-  bind() {},
-};
